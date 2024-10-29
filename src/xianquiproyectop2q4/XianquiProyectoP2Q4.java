a564584f0892cf35a1195283c0fb2506e28cf9ba
Jorge <jorgeluisas6@gmail.com>
2024-10-28 23:00:19 -0600

    cambio pequeño

--- src/xianquiproyectop2q4/XianquiProyectoP2Q4.java
+++ src/xianquiproyectop2q4/XianquiProyectoP2Q4.java
@@ -2,12 +2,20 @@
      * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
      * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
      */
+
+
     package xianquiproyectop2q4;
+
     import javax.swing.*;
     import java.awt.*;
     import java.awt.event.*;
     import java.util.ArrayList;
     import java.util.Calendar;
+
+    /**
+     *
+     * @author Jorge Aguirre
+     */
 
 public class XianquiProyectoP2Q4 {
 
